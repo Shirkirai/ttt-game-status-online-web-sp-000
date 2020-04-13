ecbcbdc761043e5d4f12c1fc5b1a33bdc98db87f
Jung Lee <lee.jung.s95@gmail.com>
2020-04-13 01:28:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,6 +64,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |sets_of_winning_combination|
+    position_taken?(board, index)
     if "#{sets_of_winning_combination}".any?{|winning_combination| winning_combination == 0}
       return "#{winning_combination}"
     else
