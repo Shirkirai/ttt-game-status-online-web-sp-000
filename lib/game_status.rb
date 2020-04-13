464ae35d51a160fb2a3127a962103fe2bf30ed0a
Jung Lee <lee.jung.s95@gmail.com>
2020-04-13 03:11:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -79,4 +79,5 @@ def won?(board)
     win_index_1 = set_of_wc[0]
     win_index_2 = set_of_wc[1]
     win_index_3 = set_of_wc[2]
-    if position_taken?(board, win_index_1) == "X" || position_taken?(board, win_index_1)"O"
+    if position_taken?(board, win_index_1) == "X" || position_taken?(board, win_index_1) == "O" && position_taken?(board, win_index_2) == "X" || position_taken?(board, win_index_2) == "O" && position_taken?(board, win_index_3) == "X" && position_taken?(board, win_index_3) == "O"
+      return set_of_wc
