dbc46a5d266306219d039ebce163696f9341a137
Jung Lee <lee.jung.s95@gmail.com>
2020-04-13 03:49:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -91,7 +91,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == [0,1,2] || won?(board) == [0,5,8]
+  if won?(board) == [0,1,2] || won?(board) == [0,4,8]
     return false
   elsif full?(board) != 0
     return true
